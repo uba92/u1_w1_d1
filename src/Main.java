4b6525fd3f625df3bd9fa930c0bdad87ee5c8e57
Umberto Tramontano <u.tramontano92@libero.it>
2025-01-07 15:05:06 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
-        int x = 1;
-        System.out.print(x);
 
 
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserisci un numero intero:");
+        int x = scanner.nextInt();
+
+        System.out.print("Inserisci un altro numero intero:");
+        int y = scanner.nextInt();
+
+        System.out.print("Il prodotto tra " + x + " e " + y + " è: " + x*y);
     }
 }
