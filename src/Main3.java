44990a01f101ba7f2fd5c260a1eecab27e61185d
Umberto Tramontano <u.tramontano92@libero.it>
2025-01-07 16:58:01 +0100

    ex3

--- src/Main3.java
+++ src/Main3.java
@@ -4,7 +4,18 @@ public class Main3 {
 
         double p = perimetro(4.2,2.5);
         System.out.println("Il perimetro del rettangolo è: " + p);
+
+        int result = pariDispari(4);
+        System.out.println(result);
     }
 
     public static double perimetro (double b, double h) {return b*2 + h*2;}
+
+    public static int pariDispari (int n) {
+        if(n%2 == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 }
