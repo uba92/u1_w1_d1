c0f67ea3f36dc2a5b316ecbb432ceefbfc8e9f8a
Umberto Tramontano <u.tramontano92@libero.it>
2025-01-07 16:49:21 +0100

    .

--- src/Main3.java
+++ src/Main3.java
@@ -2,9 +2,9 @@ public class Main3 {
     public static void main (String[] args) {
 
 
-        int p = perimetro(4,2);
+        double p = perimetro(4.2,2.5);
         System.out.println("Il perimetro del rettangolo è: " + p);
     }
 
-    public static int perimetro (int b, int h) {return b*2 + h*2;}
+    public static double perimetro (double b, double h) {return b*2 + h*2;}
 }
