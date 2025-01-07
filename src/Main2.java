52e6a5acc25d80aaf7536b662ecd3ec32d97c7e5
Umberto Tramontano <u.tramontano92@libero.it>
2025-01-07 16:39:32 +0100

    update

--- src/Main2.java
+++ src/Main2.java
@@ -2,19 +2,25 @@ import java.util.Scanner;
 
 public class Main2 {
     public static void main(String[] args) {
+
+        concatStr();
+
+
+    }
+
+    public static void concatStr () {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Inserisci una stringa");
-        String str1 = scanner.nextLine();
+        String string1 = scanner.nextLine();
 
         System.out.println("Inserisci un'altra stringa");
-        String str2 = scanner.nextLine();
+        String string2 = scanner.nextLine();
 
         System.out.println("Inserisci un'ultima stringa");
-        String str3 = scanner.nextLine();
+        String string3 = scanner.nextLine();
 
-
-        System.out.println(str1 + str2 + str3);
-        System.out.println(str3 + str2 + str1);
+        System.out.println(string1 + string2 + string3);
+        System.out.println(string3 + string2 + string1);
     }
 }
